fc443db456a5692f87c7855c62efe195825c8129
HasanC14 <chowdhuryh999@gmail.com>
2022-11-24 00:57:22 +0600

    dot env and collections added

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+//var jwt = require("jsonwebtoken");
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@back-prac-2-admin.sldkkq5.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const ProductCollection = client.db("SecondBook").collection("Products");
+    const SellerCollection = client.db("DoctorsPortal").collection("Seller");
+    const BuyerCollection = client.db("DoctorsPortal").collection("Buyer");
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
